c69d4991dc4b5e74b583a14e1763a6b736af59c4
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-01-12 10:00:39 -0500

    add test user to user list

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,7 +3,12 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [
+    {
+        username:"testuser",
+        password:"testpass"
+    }
+];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
