34fa663fc1b2dd7cfdb0d015fa3710bedbd1ca3d
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-01-11 19:47:51 -0500

    implement general route endpoints

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,6 +7,12 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+    const userAlreadyExists = users.filter((user)=> user.username === username);
+    if(!userAlreadyExists || userAlreadyExists.length === 0)
+    {
+        return true;
+    }
+    return false;
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
